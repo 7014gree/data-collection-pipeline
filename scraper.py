16bd8ab9109c0a724498ec529fb1ec42e73206b5
7014gree <7014gree@gmail.com>
2022-11-10 12:54:59 +0000

    Now navigates to links found on the first page of each category, navigates to each of those links, and updates the dictionary with information with each one. Next to add navigation to next page for each category.

--- scraper.py
+++ scraper.py
@@ -18,22 +18,67 @@ time.sleep(4)
 driver.get(link)
 grocery_categories = driver.find_elements(by=By.XPATH, value='//div[@class="mNavigationPropositionTile mNavigationBlock-desktop"]')
 list_links = []
+product_dict = {'name': [], 'short desc': [], 'price': [], 'price per unit': [], 'long desc': [], 'nutritional info': {'unit': [],
+'energy kJ': [], 'energy kcal': [], 'fat': [], 'saturates': [], 'carbohydrate': [], 'sugars': [], 'fibre': [], 'protein': [], 'salt': []}, 'url': []}
+
 for category in grocery_categories:
     category_a_tag = category.find_element(by=By.XPATH, value='./a')
     link = category_a_tag.get_attribute('href')
     time.sleep(4)
 
     driver.get(link)
-    product_grid = driver.find_element(by=By.XPATH, value='//div[@class="productLister_gridView"]')
-    product_a_tags = product_grid.find_elements(by=By.XPATH, value = './/a')
-    """for tag in product_a_tags:
-        list_links.append(tag.get_attribute('href'))
-"""
+    product_grid = driver.find_element(by=By.XPATH, value='//ul[@class="productLister gridView"]')
+    product_div_tags = product_grid.find_elements(by=By.XPATH, value='.//div[@class="productInfo"]')
+    for product_tag in product_div_tags:
+        product_link_tag = product_tag.find_element(by=By.XPATH, value='.//a')
+        list_links.append(product_link_tag.get_attribute('href'))
     break
 
+for url in list_links:
+    product_dict['url'].append(url)
+    print(url)
+    time.sleep(4)
+    driver.get(url)
+    time.sleep(10)
+    product_dict['name'].append(driver.find_element(by=By.XPATH, value='//h1[@class="pd__header"]').text)
+    product_dict['short desc'].append(driver.find_element(by=By.XPATH, value='//div[@class="pd__description"]').text)
 
-print(list_links)
-print(len(list_links))
+    price_tag = driver.find_element(by=By.XPATH, value='//div[@class="pd__cost"]')
+    product_dict['price'].append(price_tag.find_elements(by=By.XPATH, value='.//div')[1].text)
+    product_dict['price per unit'].append(price_tag.find_element(by=By.XPATH, value='.//span[@data-test-id="pd-unit-price"]').text)
+
+    product_dict['long desc'].append(driver.find_element(by=By.XPATH, value='//div[@class="productText"]').text)
+
+    nutritional_info_table = driver.find_element(by=By.XPATH, value='//table[@class="nutritionTable"]')
+    nutritional_table_rows = nutritional_info_table.find_elements(by=By.XPATH, value='.//tr')
+    
+    product_dict['nutritional info']['unit'].append(nutritional_table_rows[0].find_elements(by=By.XPATH, value='.//th[@scope="col"]')[1].text)
+    product_dict['nutritional info']['energy kJ'].append(nutritional_table_rows[1].find_elements(by=By.XPATH, value='.//td')[0].text)
+    product_dict['nutritional info']['energy kcal'].append(nutritional_table_rows[2].find_elements(by=By.XPATH, value='.//td')[0].text)
+    
+    # Fills dictionary with 0g in case the category doesn't appear on the page
+    product_dict['nutritional info']['fat'].append("0g")
+    product_dict['nutritional info']['saturates'].append("0g")
+    product_dict['nutritional info']['carbohydrate'].append("0g")
+    product_dict['nutritional info']['sugars'].append("0g")
+    product_dict['nutritional info']['fibre'].append("0g")
+    product_dict['nutritional info']['protein'].append("0g")
+    product_dict['nutritional info']['salt'].append("0g")
+
+
+    # Iterates through all of the row headings in the table and updates the relevant key upon matching
+    for row in nutritional_table_rows[3:]:
+        row_name = str.lower(row.find_element(by=By.XPATH, value='.//th').text)
+        row_amount = row.find_element(by=By.XPATH, value='.//td').text
+        print(row_name, row_amount)
+        product_dict['nutritional info'][row_name][-1] = row_amount
+
+    break
+    print(product_dict)
+    
+print(product_dict)
+
+
 
 while True:
     pass
